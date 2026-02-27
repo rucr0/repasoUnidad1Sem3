d1a08050d4b4d2fe8ce2b4af7bd3304a21007b2a
rucr0 <ruizperezdiegoruben@gmail.com>
2026-02-27 01:56:13 -0600

    Add files via upload

---
+++ Repaso1/Empleados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repasoi1
+{
+    internal class Empleados
+    {
+        int noEmpleado;
+        string nombre;
+        string apellido;
+        decimal suedoHora;
+
+        public int NoEmpleado { get => noEmpleado; set => noEmpleado = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public decimal SuedoHora { get => suedoHora; set => suedoHora = value; }
+    }
+}
---
+++ Repaso1/Reporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repasoi1
+{
+    internal class Reporte
+    {
+        string nombre;
+        string apellido;
+        decimal sueldoMes;
+        int mes;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public decimal SueldoMes { get => sueldoMes; set => sueldoMes = value; }
+        public int Mes { get => mes; set => mes = value; }
+    }
+}
